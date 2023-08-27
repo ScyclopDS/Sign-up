bcd97f0ddaf143b6d7f0d0ea64ead3b26e9989e6
Dhananjoy saha <107365888+ScyclopDS@users.noreply.github.com>
2023-08-28 02:39:52 +0530

    Add files via upload
    sing-up page using react-hook-from

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import From from'./components/From';
+
+function App() {
+  return (
+    <div className="App">
+      <From></From>  
+    </div>
+  );
+}
+
+export default App;
